05823c7274114c333f91f6bf720f45bf641b6d43
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2023-07-29 02:13:52 -0400

    modulo11

--- main.py
+++ main.py
@@ -53,8 +53,7 @@ def linha_digitavel(barcode: str):
         if modulo == 10:
             str_linha_digitavel += parte + ' ' + str(modulo10(parte)) + '   '
         else:
-            # modulo10 por equanto, ate implementar def modulo11
-            str_linha_digitavel += parte + ' ' + modulo10(parte) + '   '
+            str_linha_digitavel += parte + ' ' + str(modulo11(parte)) + '   '
 
     return str_linha_digitavel.strip()
 
@@ -102,16 +101,69 @@ def modulo10(sequencia):
             soma += resultado
         else:
             soma += int(resultado / 10)
-            soma += int(resultado % 10)
+            soma += resultado % 10
 
-    resto_divisao = int(soma % 10)
+    resto_divisao = soma % 10
     if resto_divisao == 0:
         return resto_divisao
     return 10 - resto_divisao
 
 
+def modulo11(sequencia):
+    """
+    O DAC (Dígito de Auto-Conferência) módulo 11, de um número é calculado multiplicando
+    cada algarismo, pela seqüência de multiplicadores 2,3,4,5,6,7,8,9,2,3,4....
+    posicionados da direita para a esquerda.
+
+    A soma dos produtos dessa multiplicação é dividida por 11, obtém-se o resto da
+    divisão, este resto deve ser subtraído de 11, o produto da subtração é o DAC.
+
+    Observação: Quando o resto da divisão for igual a 0 ou 1, atribuí-se ao DV o
+    digito “0”, e quando for 10, atribuí-se ao DV o digito “1”.
+
+    EXEMPLO
+
+    Calcular o DAC módulo 11 da seguinte seqüência de números: 01230067896.
+    A fórmula do cálculo é:
+
+        1. Multiplicação pela seqüência 2,3,4,5,6,7,8,9, 2,3,4 da direita
+        para a esquerda.
+              0  1  2  3  0  0  6  7  8  9  6
+            X 4  3  2  9  8  7  6  5  4  3  2
+              0  3  4 27  0  0 36 35 32 27 12
+
+        2. Soma dos produtos da multiplicação:
+            0 + 3 + 4 + 27 + 0 + 0 + 36 + 35 + 32 + 27 + 12 = 176
+
+        3. Divisão do resultado da soma acima por 11
+            176 : 11 = 16 , resto = 0
+
+    O DAC da seqüência numérica é igual a “0”.
+    """
+    multiplicador_atual = 2
+    soma = 0
+
+    for element in sequencia[::-1]:
+        resultado = int(element) * multiplicador_atual
+        multiplicador_atual = (
+            2 if multiplicador_atual == 9 else multiplicador_atual + 1
+        )
+        soma += resultado
+
+    resto_divisao = soma % 11
+    if resto_divisao in (0, 1):
+        return 0
+    if resto_divisao == 10:
+        return 1
+    return 11 - resto_divisao
+
+
 def digito_verificador_modulo10(campo1: str, campo2: str):
     return str(modulo10(campo1 + campo2))
+
+
+def digito_verificador_modulo11(campo1: str, campo2: str):
+    return str(modulo11(campo1 + campo2))
 
 
 def campo_livre(
@@ -180,6 +232,14 @@ def barcode(
         or codigo_moeda == MODULO10_QUANTIDADE_MOEDA
     ):
         digito_verificador = digito_verificador_modulo10(parte1, parte2)
+    elif (
+        codigo_moeda == MODULO11_VALOR_EFETIVO
+        or codigo_moeda == MODULO11_QUANTIDADE_MOEDA
+    ):
+        digito_verificador = digito_verificador_modulo11(parte1, parte2)
+
+    if digito_verificador == '':
+        raise Exception('Dígito verificador inválido.')
 
     numeracao += parte1 + digito_verificador + parte2
 
@@ -276,6 +336,9 @@ if __name__ == '__main__':
         vencimento=date(2023, 7, 29),
         dados_campo_livre='23300',
     )
+
+    # print(modulo10('01230067896')) # 3
+    # print(modulo11('01230067896')) # 0
 
     # print(result)
     # print(linha_digitavel(result))
