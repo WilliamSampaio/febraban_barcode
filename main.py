f71cbb904c9bd0e4f50933bb767481fd85162dba
William Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2023-07-31 23:55:05 -0400

    update readme

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from decimal import Decimal
 from febraban_barcode import (
     MODULO10_VALOR_EFETIVO,
     PRODUTO_ARRECADACAO,
+    SEGMENTO_PREFEITURA,
     barcode,
     linha_digitavel,
     modulo10,
@@ -14,40 +15,50 @@ from febraban_barcode.image import image_png, image_svg
 
 if __name__ == '__main__':
 
-    result = barcode(
+    # Gera o código de barras
+    codigo_de_barras = barcode(
         produto=PRODUTO_ARRECADACAO,
-        segmento=0,
+        segmento=SEGMENTO_PREFEITURA,
         codigo_moeda=MODULO10_VALOR_EFETIVO,
         valor=Decimal('0.01'),
-        id_empresa_orgao='0973',
+        id_empresa_orgao='4321',
         vencimento=date(2023, 7, 29),
-        dados_campo_livre='23300',
+        dados_campo_livre='123456789',
+    )
+    print(codigo_de_barras)
+
+    # Gera linha digitável
+    txt_linha_digitavel = linha_digitavel(codigo_de_barras)
+    print(txt_linha_digitavel)
+
+    # Calcula dígito módulo 10
+    print(modulo10('01230067896'))  # 3
+
+    # Calcula dígito módulo 11
+    print(modulo11('01230067896'))  # 0
+
+    # Gera o código de barras em PNG
+    image_png(
+        filename='barcode.png',
+        barcode=codigo_de_barras,
+        linha_digitavel=txt_linha_digitavel,
     )
 
-    # print(modulo10('01230067896'))   # 3
-    # print(modulo11('01230067896'))   # 0
+    # Gera o código de barras em SVG
+    image_svg(
+        filename='barcode.svg',
+        barcode=codigo_de_barras,
+        linha_digitavel=txt_linha_digitavel,
+    )
 
-    # print(result)
-    # print(linha_digitavel(result))
+    # Gera o código de barras em base64 PNG para utilização em html
+    html_base64_teste(
+        'index1.html',
+        base64_png(codigo_de_barras, linha_digitavel=txt_linha_digitavel),
+    )
 
-    # image_png(
-    #     filename='teste.png',
-    #     barcode=result,
-    #     linha_digitavel=linha_digitavel(result),
-    # )
-
-    # image_svg(
-    #     filename='teste.svg',
-    #     barcode=result,
-    #     linha_digitavel=linha_digitavel(result),
-    # )
-
-    # html_base64_teste(
-    #     'index1.html',
-    #     base64_png(result, linha_digitavel=linha_digitavel(result)),
-    # )
-
-    # html_base64_teste(
-    #     'index2.html',
-    #     base64_svg(result, linha_digitavel=linha_digitavel(result)),
-    # )
+    # Gera o código de barras em base64 SVG para utilização em html
+    html_base64_teste(
+        'index2.html',
+        base64_svg(codigo_de_barras, linha_digitavel=txt_linha_digitavel),
+    )
