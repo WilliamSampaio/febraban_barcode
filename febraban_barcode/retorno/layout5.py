c333034b8b3df3470a1cf0f2e42051b03446b6d9
William B M Sampaio <56176702+WilliamSampaio@users.noreply.github.com>
2023-12-25 01:59:16 -0400

    update

--- febraban_barcode/retorno/layout5.py
+++ febraban_barcode/retorno/layout5.py
@@ -1,41 +1,41 @@
 from datetime import datetime
 
 
-def header(
-    A01='A',
-    A02='2',
-    A03=' ',
-    A04=' ',
-    A05='0',
-    A06=' ',
-    A07='0',
-    A08='0',
-    A09='05',
-    A10='CODIGO DE BARRAS',
-    A11=' ',
+def registro_A(
+    codigo_registro='A',
+    codigo_remessa='2',
+    codigo_convenio=' ',
+    nome_empresa=' ',
+    codigo_banco='0',
+    nome_banco=' ',
+    data_geracao='',
+    nsa='0',
+    versao_layout='05',
+    servico='CODIGO DE BARRAS',
+    reservado=' ',
 ):
     """
     REGISTRO “A” – HEADER
 
     Args:
-        A01 (str, optional): Código do registro = “A”. Defaults to 'A'.
-        A02 (str, optional): Código de Remessa
+        codigo_registro (str, optional): A.01 - Código do registro = “A”. Defaults to 'A'.
+        codigo_remessa (str, optional): A.02 - Código de Remessa
             2 - RETORNO - Enviado pelo Banco para a Empresa/Órgão. Defaults to '2'.
-        A03 (str, optional): Código do Convênio
+        codigo_convenio (str, optional): A.03 - Código do Convênio
             Definido pelo banco. Defaults to ' '.
-        A04 (str, optional): Nome da Empresa/Órgão. Defaults to ' '.
-        A05 (str, optional): Código do Banco
+        nome_empresa (str, optional): A.04 - Nome da Empresa/Órgão. Defaults to ' '.
+        codigo_banco (str, optional): A.05 - Código do Banco
             Código do Banco na câmara de compensação. Defaults to '0'.
-        A06 (str, optional): Nome do Banco. Defaults to ' '.
-        A07 (str, optional): Data da geração do arquivo (AAAAMMDD). Defaults to '0'.
-        A08 (str, optional): Número seqüencial do arquivo ( NSA )
+        nome_banco (str, optional): A.06 - Nome do Banco. Defaults to ' '.
+        data_geracao (str, optional): A.07 - Data da geração do arquivo (AAAAMMDD). Defaults to '0'.
+        nsa (str, optional): A.08 - Número seqüencial do arquivo ( NSA )
             Este número deverá evoluir de 1 em 1 para cada arquivo gerado. Defaults to '0'.
-        A09 (str, optional): Versão do lay - out
+        versao_layout (str, optional): A.09 - Versão do lay - out
             atual = versão 04
             nova = versão 05 - disponível a partir de 01.08.2016. Defaults to '05'.
-        A10 (str, optional): Identificação do serviço
+        servico (str, optional): A.10 - Identificação do serviço
             Deverá conter “CÓDIGO DE BARRAS”. Defaults to 'CODIGO DE BARRAS'.
-        A11 (str, optional): Reservado para o futuro (filler). Defaults to ' '.
+        reservado (str, optional): A.11 - Reservado para o futuro (filler). Defaults to ' '.
 
     Raises:
         Exception: Campo inválido.
@@ -45,75 +45,75 @@ def header(
     """
     header_str = ''
 
-    if len(A01) != 1:
+    if len(codigo_registro) != 1:
         raise Exception('Campo A.01 Inválido. O campo deve ter o tamanho 1.')
-    header_str += A01
+    header_str += codigo_registro
 
-    if len(A02) != 1:
+    if len(codigo_remessa) != 1:
         raise Exception('Campo A.02 Inválido. O campo deve ter o tamanho 1.')
-    if not A02.isdigit():
+    if not codigo_remessa.isdigit():
         raise Exception('Campo A.02 Inválido. O campo deve ser numérico.')
-    header_str += A02
+    header_str += codigo_remessa
 
-    if len(A03) > 20:
+    if len(codigo_convenio) > 20:
         raise Exception(
             'Campo A.03 Inválido. O campo deve ter o tamanho máximo de 20.'
         )
-    header_str += A03.ljust(20)
+    header_str += codigo_convenio.ljust(20)
 
-    if len(A04) > 20:
+    if len(nome_empresa) > 20:
         raise Exception(
             'Campo A.04 Inválido. O campo deve ter o tamanho máximo de 20.'
         )
-    header_str += A04.ljust(20)
+    header_str += nome_empresa.ljust(20)
 
-    if len(A05) > 3:
+    if len(codigo_banco) > 3:
         raise Exception(
             'Campo A.05 Inválido. O campo deve ter o tamanho máximo de 3.'
         )
-    if not A05.isdigit():
+    if not codigo_banco.isdigit():
         raise Exception('Campo A.05 Inválido. O campo deve ser numérico.')
-    header_str += A05.zfill(3)
+    header_str += codigo_banco.zfill(3)
 
-    if len(A06) > 20:
+    if len(nome_banco) > 20:
         raise Exception(
             'Campo A.06 Inválido. O campo deve ter o tamanho máximo de 20.'
         )
-    header_str += A06.upper().ljust(20)
+    header_str += nome_banco.upper().ljust(20)
 
     try:
-        datetime.strptime(A07, '%Y%m%d')
+        datetime.strptime(data_geracao, '%Y%m%d')
     except:
         raise Exception(
-            'Campo A.07 Inválido. O deve ser uma data no formato AAAAMMDD.'
+            'Campo A.07 Inválido. O campo deve ser uma data no formato AAAAMMDD.'
         )
-    header_str += A07
+    header_str += data_geracao
 
-    if len(A08) > 6:
+    if len(nsa) > 6:
         raise Exception(
             'Campo A.08 Inválido. O campo deve ter o tamanho máximo de 6.'
         )
-    if not A08.isdigit():
+    if not nsa.isdigit():
         raise Exception('Campo A.08 Inválido. O campo deve ser numérico.')
-    header_str += A08.zfill(6)
+    header_str += nsa.zfill(6)
 
-    if len(A09) > 2:
+    if len(versao_layout) > 2:
         raise Exception(
             'Campo A.09 Inválido. O campo deve ter o tamanho máximo de 2.'
         )
-    if not A09.isdigit():
+    if not versao_layout.isdigit():
         raise Exception('Campo A.09 Inválido. O campo deve ser numérico.')
-    header_str += A09.zfill(2)
+    header_str += versao_layout.zfill(2)
 
-    if len(A10) > 17:
+    if len(servico) > 17:
         raise Exception(
             'Campo A.10 Inválido. O campo deve ter o tamanho máximo de 17.'
         )
-    header_str += A10.upper().ljust(17)
+    header_str += servico.upper().ljust(17)
 
-    if len(A11) > 52:
+    if len(reservado) > 52:
         raise Exception(
             'Campo A.11 Inválido. O campo deve ter o tamanho máximo de 52.'
         )
-    header_str += A11.upper().ljust(52)
+    header_str += reservado.upper().ljust(52)
     return header_str
